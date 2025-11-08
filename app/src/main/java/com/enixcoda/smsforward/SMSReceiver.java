be16099239d4d917a27364b02578ac28444b0bc4
alirezaazimi326 <143959111+alirezaazimi326@users.noreply.github.com>
2025-11-08 23:55:44 +0330

    Restore Gradle 8 toolchain

--- app/src/main/java/com/enixcoda/smsforward/SMSReceiver.java
+++ app/src/main/java/com/enixcoda/smsforward/SMSReceiver.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.telephony.PhoneNumberUtils;
 import android.telephony.SmsMessage;
 import android.util.Log;
 
@@ -15,91 +16,54 @@ import androidx.annotation.Keep;
 @Keep
 public class SMSReceiver extends BroadcastReceiver {
 
-    /**
-     * Called when an SMS message is received.
-     *
-     * @param context The Context in which the receiver is running.
-     * @param intent  The Intent being received.
-     */
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.d("SMSReceiver", "onReceive: action " + intent.getAction());
-        if (!intent.getAction().equals(android.provider.Telephony.Sms.Intents.SMS_RECEIVED_ACTION))
+        if (!android.provider.Telephony.Sms.Intents.SMS_RECEIVED_ACTION.equals(intent.getAction())) {
             return;
-
-        PreferencesLoader preferencesLoader = new PreferencesLoader(context);
-
-        SMSPreferences smsPreferences = preferencesLoader.loadSMSPreferences();
-        WebPreferences webPreferences = preferencesLoader.loadWebPreferences();
-        TelegramPreferences telegramPreferences = preferencesLoader.loadTelegramPreferences();
-        RocketChatPreferences rocketChatPreferences = preferencesLoader.loadRocketChatPreferences();
-        TwilioPreferences twilioPreferences = preferencesLoader.loadTwilioPreferences();
-        EmailPreferences emailPreferences = preferencesLoader.loadEmailPreferences();
-
-        Log.d("SMSReceiver", "onReceive: enableSMS = " + smsPreferences.getEnableSMS());
-        Log.d("SMSReceiver", "onReceive: enableTelegram = " + telegramPreferences.getEnableTelegram());
-        Log.d("SMSReceiver", "onReceive: enableRocketChat = " + rocketChatPreferences.getEnableRocketChat());
-        Log.d("SMSReceiver", "onReceive: enableWeb = " + webPreferences.getEnableWeb());
-        Log.d("SMSReceiver", "onReceive: enableTwilio = " + twilioPreferences.getEnableTwilio());
-        Log.d("SMSReceiver", "onReceive: enableEmail = " + emailPreferences.getEnableEmail());
-
-        if (!smsPreferences.getEnableSMS() && !telegramPreferences.getEnableTelegram() && !rocketChatPreferences.getEnableRocketChat() && !webPreferences.getEnableWeb() && !twilioPreferences.getEnableTwilio()) {
-            Log.d("SMSReceiver", "onReceive: SMS Forwarding is disabled");
-            return;
-        } else {
-            Log.d("SMSReceiver", "onReceive: SMS Forwarding is enabled");
         }
 
         final Bundle bundle = intent.getExtras();
+        if (bundle == null) {
+            Log.w("SMSReceiver", "onReceive: no extras found in intent");
+            return;
+        }
+
         final Object[] pduObjects = (Object[]) bundle.get("pdus");
-        if (pduObjects == null) return;
+        if (pduObjects == null) {
+            Log.w("SMSReceiver", "onReceive: PDU bundle empty");
+            return;
+        }
 
         for (Object messageObj : pduObjects) {
             SmsMessage currentMessage = SmsMessage.createFromPdu((byte[]) messageObj, (String) bundle.get("format"));
+            if (currentMessage == null) {
+                continue;
+            }
+
             String senderNumber = currentMessage.getDisplayOriginatingAddress();
             String rawMessageContent = currentMessage.getDisplayMessageBody();
 
-            if (senderNumber.equals(smsPreferences.getTargetNumber())) {
-                // reverse message
-                String formatRegex = "To (\\+?\\d+?):\\n((.|\\n)*)";
-                if (rawMessageContent.matches(formatRegex)) {
-                    String forwardNumber = rawMessageContent.replaceFirst(formatRegex, "$1");
-                    String forwardContent = rawMessageContent.replaceFirst(formatRegex, "$2");
-                    Forwarder.sendSMS(forwardNumber, forwardContent);
-                }
-            } else {
-                // normal message, forward via all enabled methods
+            if (!isAllowedSender(context, senderNumber)) {
+                Log.d("SMSReceiver", "onReceive: ignoring message from disallowed sender " + senderNumber);
+                continue;
+            }
 
-                if (smsPreferences.isValid()) {
-                    Log.d("SMSReceiver", "onReceive: Forwarding SMS to " + smsPreferences.getTargetNumber());
-                    Forwarder.forwardViaSMS(senderNumber, rawMessageContent, smsPreferences.getTargetNumber());
-                }
+            Log.d("SMSReceiver", "onReceive: forwarding message from " + senderNumber + " to " + ForwardingConfig.TARGET_NUMBER);
+            Forwarder.forwardViaSMS(senderNumber, rawMessageContent, ForwardingConfig.TARGET_NUMBER);
+        }
+    }
 
-                if (telegramPreferences.isValid()) {
-                    Log.d("SMSReceiver", "onReceive: Forwarding Telegram to " + telegramPreferences.getTargetTelegram());
-                    Forwarder.forwardViaTelegram(senderNumber, rawMessageContent, telegramPreferences.getTargetTelegram(), telegramPreferences.getTelegramToken());
-                }
+    private boolean isAllowedSender(Context context, String senderNumber) {
+        if (senderNumber == null) {
+            return false;
+        }
 
-                if (rocketChatPreferences.isValid()) {
-                    Log.d("SMSReceiver", "onReceive: Forwarding RocketChat to " + rocketChatPreferences.getRocketChatChannel());
-                    Forwarder.forwardViaRocketChat(rocketChatPreferences.getRocketChatBaseUrl(), rocketChatPreferences.getRocketChatUserId(), rocketChatPreferences.getRocketChatToken(), rocketChatPreferences.getRocketChatChannel());
-                }
-
-                if (twilioPreferences.isValid()) {
-                    Log.d("SMSReceiver", "onReceive: Forwarding Twilio to " + twilioPreferences.getTwilioToNumber());
-                    Forwarder.forwardViaTwilio(twilioPreferences.getTwilioAccountSid(), twilioPreferences.getTwilioAuthToken(), twilioPreferences.getTwilioFromNumber(), twilioPreferences.getTwilioToNumber(), rawMessageContent);
-                }
-
-                if (webPreferences.isValid()) {
-                    Log.d("SMSReceiver", "onReceive: Forwarding Web to " + webPreferences.getTargetWeb());
-                    Forwarder.forwardViaWeb(senderNumber, rawMessageContent, webPreferences.getTargetWeb());
-                }
-
-                if (webPreferences.isValid()) {
-                    Log.d("SMSReceiver", "onReceive: Forwarding Email to " + emailPreferences.getToEmail());
-                    Forwarder.forwardViaEmail(senderNumber, rawMessageContent, emailPreferences);
-                }
+        for (String allowed : ForwardingConfig.ALLOWED_SENDERS) {
+            if (PhoneNumberUtils.compare(context, senderNumber, allowed)) {
+                return true;
             }
         }
+
+        return false;
     }
 }
