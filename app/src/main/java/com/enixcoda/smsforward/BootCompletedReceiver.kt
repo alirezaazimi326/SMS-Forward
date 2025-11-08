03dcf014d2f115937258df187adedaeddb7dcb54
alirezaazimi326 <143959111+alirezaazimi326@users.noreply.github.com>
2025-11-08 22:36:34 +0330

    Update Gradle stack for modern JDK

---
+++ app/src/main/java/com/enixcoda/smsforward/BootCompletedReceiver.kt
@@ -0,0 +1,22 @@
+package com.enixcoda.smsforward
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.core.content.ContextCompat
+
+class BootCompletedReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action == Intent.ACTION_BOOT_COMPLETED ||
+            intent.action == Intent.ACTION_LOCKED_BOOT_COMPLETED ||
+            intent.action == Intent.ACTION_MY_PACKAGE_REPLACED) {
+            val serviceIntent = Intent(context, SMSForwardForegroundService::class.java)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                ContextCompat.startForegroundService(context, serviceIntent)
+            } else {
+                context.startService(serviceIntent)
+            }
+        }
+    }
+}
